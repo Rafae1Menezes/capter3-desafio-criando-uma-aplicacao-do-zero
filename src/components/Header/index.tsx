ef517d17a182d44c64560a00fabcf23f8452481a
Rafael Menezes <menezes.inbox@gmail.com>
2021-12-31 14:34:48 -0300

    integra pagina slug com prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
-    <>
-      <img src="/logo.svg" alt="logo" className={styles.logo} />
-    </>
+    <Link href="/" passHref>
+      <a>
+        <img src="/logo.svg" alt="logo" className={styles.logo} />
+      </a>
+    </Link>
   );
 }
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,13 +1,16 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { RichText } from 'prismic-dom';
+
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
-
-import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 
 interface Post {
   first_publication_date: string | null;
@@ -30,7 +33,7 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ Post }: PostProps): JSX.Element {
+export default function Post({ post }: PostProps): JSX.Element {
   return (
     <>
       <div className={commonStyles.container}>
@@ -43,75 +46,62 @@ export default function Post({ Post }: PostProps): JSX.Element {
 
       <div className={commonStyles.container}>
         <article className={styles.article}>
-          <h1>Criando um app CRA do zero</h1>
+          <h1>{post.data.title}</h1>
           <div className={styles.info}>
-            <FiCalendar /> 15 Mar 2021
-            <FiUser /> Joseph Oliveira
+            <FiCalendar /> {post.first_publication_date}
+            <FiUser /> {post.data.author}
             <FiClock /> 4 min
           </div>
 
-          <h2>Proin et varius</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <p>
-            Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit
-            tellus. Nam facilisis sodales felis, pharetra pharetra lectus auctor
-            sed.
-          </p>
-          <p>
-            Ut venenatis mauris vel libero pretium, et pretium ligula faucibus.
-            Morbi nibh felis, elementum a posuere et, vulputate et erat. Nam
-            venenatis.
-          </p>
-
-          <h2>Cras laoreet mi</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.{' '}
-            <a href="#">Laboriosam veniam dicta ad voluptatibus</a> delectus
-            fugiat natus, aliquam perspiciatis! Dolorem commodi alias maiores
-            asperiores iusto qui, cupiditate ad ut nihil et! delectus fugiat
-            natus, aliquam perspiciatis! Dolorem commodi alias maiores
-            asperiores iusto qui, cupiditate ad ut nihil et! delectus fugiat
-            natus, aliquam perspiciatis! Dolorem commodi alias maiores
-            asperiores iusto qui, cupiditate ad ut nihil et!
-          </p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam
-            veniam dicta ad voluptatibus delectus fugiat natus, aliquam
-            perspiciatis! Dolorem commodi alias maiores asperiores iusto qui,
-            cupiditate ad ut nihil et! aliquam perspiciatis! Dolorem commodi
-            alias maiores asperiores iusto qui, cupiditate ad ut nihil et!
-            delectus fugiat natus, aliquam perspiciatis! Dolorem commodi alias
-            maiores asperiores iusto qui, cupiditate ad ut nihil et! delectus
-            fugiat natus, aliquam perspiciatis! Dolorem commodi alias maiores
-            asperiores iusto qui, cupiditate ad ut nihil et!
-          </p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam
-            veniam dicta ad voluptatibus delectus fugiat natus, aliquam
-            perspiciatis! Dolorem commodi alias maiores asperiores iusto qui,
-            cupiditate ad ut nihil et! aliquam perspiciatis! Dolorem commodi
-            alias maiores asperiores iusto qui, cupiditate ad ut nihil et!
-            delectus fugiat natus, aliquam perspiciatis! Dolorem commodi alias
-            maiores asperiores iusto qui, cupiditate ad ut nihil et! delectus
-            fugiat natus, aliquam perspiciatis! Dolorem commodi alias maiores
-            asperiores iusto qui, cupiditate ad ut nihil et!
-          </p>
+          {post.data.content.map((part, index) => (
+            <div key={part.heading}>
+              <h2>{part.heading}</h2>
+              <div
+                dangerouslySetInnerHTML={{ __html: RichText.asHtml(part.body) }}
+              />
+            </div>
+          ))}
         </article>
       </div>
     </>
   );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths = async () => {
+  //   const prismic = getPrismicClient();
+  //   const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const response = await prismic.getByUID('post', slug, {});
+
+  const post: Post = {
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      'd LLL yyyy',
+      { locale: ptBR }
+    ),
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.author1.url,
+      },
+      author: response.data.author,
+      content: response.data.content,
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
