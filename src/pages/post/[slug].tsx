a69f8a6ec85dcedd9dc79dfa2696753e325c9ad2
Rafael Menezes <menezes.inbox@gmail.com>
2021-12-31 15:16:19 -0300

    cria contagem de tempo

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -12,6 +12,19 @@ import { getPrismicClient } from '../../services/prismic';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
+function calcTime(content): string {
+  const totalPalavras = content.reduce((acc, actual) => {
+    const text = RichText.asText(actual.body);
+    const tamanho = text.split(/(\w+)/g).length;
+
+    return acc + tamanho;
+  }, 0);
+
+  const minutos = Math.ceil(totalPalavras / 200);
+
+  return `${minutos} minutos`;
+}
+
 interface Post {
   first_publication_date: string | null;
   data: {
@@ -50,12 +63,11 @@ export default function Post({ post }: PostProps): JSX.Element {
           <div className={styles.info}>
             <FiCalendar /> {post.first_publication_date}
             <FiUser /> {post.data.author}
-            <FiClock /> 4 min
+            <FiClock /> {calcTime(post.data.content)}
           </div>
 
-          {post.data.content.map((part, index) => (
+          {post.data.content.map(part => (
             <div key={part.heading}>
-              {console.log(part)}
               <h2>{part.heading}</h2>
               <div
                 dangerouslySetInnerHTML={{ __html: RichText.asHtml(part.body) }}
@@ -68,7 +80,7 @@ export default function Post({ post }: PostProps): JSX.Element {
   );
 }
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   //   const prismic = getPrismicClient();
   //   const posts = await prismic.query(TODO);
 
@@ -82,7 +94,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug } = params;
   const prismic = getPrismicClient();
 
-  const response = await prismic.getByUID('post', slug, {});
+  const response = await prismic.getByUID('post', `${slug}`, {});
 
   const post: Post = {
     first_publication_date: format(
